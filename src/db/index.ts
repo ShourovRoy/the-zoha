54f92d45246b275193ccf9cc1993fb6d9f14c3ce
Shourov Roy <shourovroy2389@gmail.com>
2026-03-05 17:31:47 +0600

    node added

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,6 +5,11 @@ import * as schema from './schema/index'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : undefined,
 })
+
+console.log('poo;: ', pool)
 export const db = drizzle({ client: pool, schema: schema })
